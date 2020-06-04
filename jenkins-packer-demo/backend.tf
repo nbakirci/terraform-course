2424ddf8ad8d38396d346b5366f51f30f0151ec3
nbakirci <45741807+nbakirci@users.noreply.github.com>
2020-06-04 00:23:11 -0400

    add back #

--- jenkins-packer-demo/backend.tf
+++ jenkins-packer-demo/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-bp110qzm"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-state-bp110qzm"
+#    key    = "terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
